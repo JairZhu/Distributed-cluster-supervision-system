8fc9821164b1836dab79296bdfab9888e76fe345
JairZhu <739741104@qq.com>
2020-12-27 22:51:31 +0800

    新增“显示信息”页面，功能基本完成

--- dcss-ui/src/components/Information.vue
+++ dcss-ui/src/components/Information.vue
@@ -1,6 +1,65 @@
 <template>
   <div>
-    <span>显示信息</span>
+    <el-card style="margin-bottom: 10px;">
+      <div slot="header">
+        <span>Server Logic View</span>
+      </div>
+      <div>
+        <el-image
+          style="width: 600px;height: 400px"
+          :src="'./static/Server Logic View.png'"
+          :fit="'contain'"
+        ></el-image>
+      </div>
+    </el-card>
+    <el-card style="margin-bottom: 10px;">
+      <div slot="header">
+        <span>Machine Logic View</span>
+      </div>
+      <div>
+        <el-image
+          style="width: 600px;height: 400px"
+          :src="'./static/Machine Logic View.png'"
+          :fit="'contain'"
+        ></el-image>
+      </div>
+    </el-card>
+    <el-card style="margin-bottom: 10px;">
+      <div slot="header">
+        <span>Process View</span>
+      </div>
+      <div>
+        <el-image
+          style="width: 600px;height: 400px"
+          :src="'./static/Process View.png'"
+          :fit="'contain'"
+        ></el-image>
+      </div>
+    </el-card>
+    <el-card style="margin-bottom: 10px;">
+      <div slot="header">
+        <span>Deployment View</span>
+      </div>
+      <div>
+        <el-image
+          style="width: 600px;height: 400px"
+          :src="'./static/Deployment View.png'"
+          :fit="'contain'"
+        ></el-image>
+      </div>
+    </el-card>
+    <el-card style="margin-bottom: 10px;">
+      <div slot="header">
+        <span>Development View</span>
+      </div>
+      <div>
+        <el-image
+          style="width: 600px;height: 400px"
+          :src="'./static/Development View.png'"
+          :fit="'contain'"
+        ></el-image>
+      </div>
+    </el-card>
   </div>
 </template>
 
